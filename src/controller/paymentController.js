e4c1152a64cd383218d1823c3661aba2a7d131e3
chandangupta1997 <chandanguptajobs@gmail.com>
2022-10-03 13:01:46 +0530

    stripe payments

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -2,9 +2,26 @@
 
 const paymentModel = require('../models/paymentModel')
 
+const orderModel =require('../models/orderModel')
 
 
 const makePayment = async function(req,res){
+
+    // we will check how much payment for the order by order Id
+
+    let orderId = req.params.orderId
+
+  
+
+    // wiil check amount 
+    let orderAmount = await orderModel.findById(orderId)
+    if(!orderAmount){
+        return res.send(' no order Amount ')
+    }
+
+    // will be making axios call to the backend server 
+    // Stripe Payments 
+
     
 
 
